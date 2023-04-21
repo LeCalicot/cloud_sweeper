feb5ea77a915b1f081b5fa7f35424512f1b332ca
Luc Couturier <luccouturier@mailfence.com>
2023-04-21 21:58:56 +0200

    [FIX] Compile but error at runtime

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
 
 use bevy::prelude::*;
+use bevy::window::{PresentMode, PrimaryWindow};
 // use bevy::prelude::{
 //     App, ClearColor, Color, ImagePlugin, Msaa, NonSend, PluginGroup, WindowDescriptor,
 // };
 // use bevy::render::texture::ImageSettings;
-use bevy::window::{WindowId, WindowPlugin};
 use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
+use bevy::{
+    prelude::*,
+    window::{Window, WindowPlugin},
+};
 use cloud_sweeper::GamePlugin;
 use std::io::Cursor;
-use winit::window::Icon;
-
+use winit::window::{Icon, WindowId};
 // TODO: bug, when doing special keep cooldown texture
 // TODO: bug, when in a corner, doesn't detect lose condition
 
@@ -31,19 +34,26 @@ pub const DISPLAY_RATIO: f32 = 1. / 4.;
 
 fn main() {
     App::new()
-        .insert_resource(Msaa { samples: 1 })
+        // .insert_resource(Msaa { samples: 1 })
         .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
         .add_plugins(
             DefaultPlugins
                 .set(ImagePlugin::default_nearest())
                 .set(WindowPlugin {
-                    window: WindowDescriptor {
-                        width: 10.5 * TILE_SIZE / DISPLAY_RATIO,
-                        height: 10. * TILE_SIZE / DISPLAY_RATIO,
-                        title: "Cloud Sweeper".to_string(), // ToDo
-                        canvas: Some("#bevy".to_owned()),
+                    primary_window: Some(Window {
+                        title: "Cloud Sweeper".into(),
+                        resolution: (
+                            10.5 * TILE_SIZE / DISPLAY_RATIO,
+                            10. * TILE_SIZE / DISPLAY_RATIO,
+                        )
+                            .into(),
+                        present_mode: PresentMode::AutoVsync,
+                        // Tells wasm to resize the window according to the available canvas
+                        fit_canvas_to_parent: true,
+                        // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                        prevent_default_event_handling: false,
                         ..default()
-                    },
+                    }),
                     ..default()
                 }),
         )
@@ -53,14 +63,24 @@ fn main() {
 }
 
 // Sets the icon on windows and X11
-fn set_window_icon(windows: NonSend<WinitWindows>) {
-    let primary = windows.get_window(WindowId::primary()).unwrap();
+fn set_window_icon(
+    primary_query: Query<Entity, With<PrimaryWindow>>,
+    windows: NonSend<WinitWindows>,
+) {
+    let Ok(entity) = primary_query.get_single() else {
+        return;
+    };
+    let Some(primary) = windows.get_window(entity) else {
+        return;
+    };
+
     let icon_buf = Cursor::new(include_bytes!("../assets/textures/app_icon.png"));
     if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
         let image = image.into_rgba8();
         let (width, height) = image.dimensions();
         let rgba = image.into_raw();
         let icon = Icon::from_rgba(rgba, width, height).unwrap();
+
         primary.set_window_icon(Some(icon));
-    };
+    }
 }
