f2380139357759c9f59a969c90823608385478fb
Luc Couturier <luccouturier@mailfence.com>
2023-10-08 00:00:50 +0200

    Finish splash screen

--- src/splashscreen.rs
+++ src/splashscreen.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 use bevy::prelude::*;
 use bevy_easings::EaseFunction;
-use bevy_splash_screen::{SplashAssetType, SplashItem, SplashPlugin, SplashScreen};
+use bevy_splash_screen::{SplashAssetType, SplashItem, SplashPlugin, SplashScreen, SplashType};
 
 use crate::{world::DISPLAY_RATIO, GameState};
 
@@ -22,26 +22,18 @@ impl Plugin for SplashscreenPlugin {
                             asset: SplashAssetType::SingleText(
                                 Text::from_sections([
                                     TextSection::new(
-                                        "Simple Test\n",
+                                        "Created by\n",
                                         TextStyle {
                                             font_size: 40.,
-                                            color: Color::WHITE,
-                                            ..default()
-                                        },
-                                    ),
-                                    TextSection::new(
-                                        "by\n",
-                                        TextStyle {
-                                            font_size: 24.,
-                                            color: Color::WHITE.with_a(0.75),
+                                            color: Color::GOLD,
                                             ..default()
                                         },
                                     ),
                                     TextSection::new(
                                         "Le Calicot",
                                         TextStyle {
-                                            font_size: 32.,
-                                            color: Color::WHITE,
+                                            font_size: 60.,
+                                            color: Color::GOLD,
                                             ..default()
                                         },
                                     ),
@@ -49,11 +41,91 @@ impl Plugin for SplashscreenPlugin {
                                 .with_alignment(TextAlignment::Center),
                                 "fonts/FiraSans-Bold.ttf".to_string(),
                             ),
-                            tint: Color::SEA_GREEN,
+                            tint: Color::GOLD,
                             width: Val::Percent(30.),
                             height: Val::Px(150.),
                             ease_function: EaseFunction::QuarticInOut.into(),
-                            duration: Duration::from_secs_f32(2.),
+                            duration: Duration::from_secs_f32(4.),
+                            is_static: false,
+                        },
+                        SplashItem {
+                            asset: SplashAssetType::SingleText(
+                                Text::from_sections([
+                                    TextSection::new(
+                                        "Pixel Art by\n",
+                                        TextStyle {
+                                            font_size: 40.,
+                                            color: Color::GOLD,
+                                            ..default()
+                                        },
+                                    ),
+                                    TextSection::new(
+                                        "Dezilim",
+                                        TextStyle {
+                                            font_size: 60.,
+                                            color: Color::GOLD,
+                                            ..default()
+                                        },
+                                    ),
+                                ])
+                                .with_alignment(TextAlignment::Center),
+                                "fonts/FiraSans-Bold.ttf".to_string(),
+                            ),
+                            tint: Color::GOLD,
+                            width: Val::Percent(30.),
+                            height: Val::Px(150.),
+                            ease_function: EaseFunction::QuarticInOut.into(),
+                            duration: Duration::from_secs_f32(4.5),
+                            is_static: false,
+                        },
+                        SplashItem {
+                            asset: SplashAssetType::SingleText(
+                                Text::from_sections([
+                                    TextSection::new(
+                                        "Music by\n",
+                                        TextStyle {
+                                            font_size: 40.,
+                                            color: Color::GOLD,
+                                            ..default()
+                                        },
+                                    ),
+                                    TextSection::new(
+                                        "Hstick",
+                                        TextStyle {
+                                            font_size: 60.,
+                                            color: Color::GOLD,
+                                            ..default()
+                                        },
+                                    ),
+                                ])
+                                .with_alignment(TextAlignment::Center),
+                                "fonts/FiraSans-Bold.ttf".to_string(),
+                            ),
+                            tint: Color::GOLD,
+                            width: Val::Percent(30.),
+                            height: Val::Px(150.),
+                            ease_function: EaseFunction::QuarticInOut.into(),
+                            duration: Duration::from_secs_f32(5.),
+                            is_static: false,
+                        },
+                        SplashItem {
+                            asset: SplashAssetType::SingleText(
+                                Text::from_sections([TextSection::new(
+                                    "Created with",
+                                    TextStyle {
+                                        font_size: 40.,
+                                        color: Color::SILVER,
+                                        ..default()
+                                    },
+                                )])
+                                .with_alignment(TextAlignment::Center),
+                                "fonts/FiraSans-Bold.ttf".to_string(),
+                            ),
+                            tint: Color::SILVER,
+                            width: Val::Percent(30.),
+                            height: Val::Px(150.),
+                            ease_function: EaseFunction::QuarticInOut.into(),
+                            duration: Duration::from_secs_f32(5.5),
                             is_static: false,
                         },
                         SplashItem {
@@ -61,13 +133,39 @@ impl Plugin for SplashscreenPlugin {
                                 "textures/bevy_logo.png".to_string(),
                             ),
                             tint: Color::WHITE,
-                            width: Val::Px(1000.),
-                            height: Val::Px(250.),
+                            width: Val::Px(500.),
+                            height: Val::Px(125.),
                             ease_function: EaseFunction::QuinticInOut.into(),
-                            duration: Duration::from_secs_f32(3.),
+                            duration: Duration::from_secs_f32(5.5),
                             is_static: true,
                         },
                     ],
+                    splash_type: SplashType::Grid,
+                    background_color: BackgroundColor(Color::BLACK),
+                    ..default()
+                })
+                .add_screen(SplashScreen {
+                    brands: vec![SplashItem {
+                        asset: SplashAssetType::SingleText(
+                            Text::from_sections([TextSection::new(
+                                "Cloud Sweeper\n",
+                                TextStyle {
+                                    font_size: 200.,
+                                    color: Color::GOLD,
+                                    ..default()
+                                },
+                            )])
+                            .with_alignment(TextAlignment::Center),
+                            "fonts/FiraSans-Bold.ttf".to_string(),
+                        ),
+                        tint: Color::SALMON,
+                        width: Val::Percent(40.),
+                        height: Val::Percent(50.),
+                        ease_function: EaseFunction::QuarticInOut.into(),
+                        duration: Duration::from_secs_f32(4.),
+                        is_static: false,
+                    }],
+                    splash_type: SplashType::Grid,
                     background_color: BackgroundColor(Color::BLACK),
                     ..default()
                 }),
